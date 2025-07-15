f270149d627e6bdd7481701b048a2a46b4e55993
HugoLePicard <lepicardhugo@gmail.com>
2025-07-15 14:56:26 +0000

    added utils

---
+++ utils/constants.py
@@ -0,0 +1,1 @@
+DIFFUSION_MODEL_ID = "stable-diffusion-v1-5/stable-diffusion-v1-5"
---
+++ utils/paths.py
@@ -0,0 +1,4 @@
+import os
+
+BASE_PATH       = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+MODELS_DIR_PATH = os.path.join(BASE_PATH, "data", "models")
